443772a03bb8cba9f7d4b2b0635f55899176781f
Образцов Григорий <grobikon@mail.ru>
2021-12-09 00:14:29 +0300

    Create  Spring Cloud Gateway

---
+++ api-gateway-service/src/test/kotlin/ru/grobikon/apigatewayservice/ApiGatewayServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.grobikon.apigatewayservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
